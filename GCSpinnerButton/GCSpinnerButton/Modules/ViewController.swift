19ba790470cf0a3fe245f9f0902be25de7fa5571
Guilherme <guizin.coelho@gmail.com>
2018-07-30 13:28:17 -0300

    Creta xcode project -- add gitignore

---
+++ GCSpinnerButton/GCSpinnerButton/Modules/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GCSpinnerButton
+//
+//  Created by Guilherme Coelho on 30/07/18.
+//  Copyright © 2018 DevForFun_Guilherme_Coelho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
